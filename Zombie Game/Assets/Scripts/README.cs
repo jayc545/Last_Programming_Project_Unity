fa25c54534e7edd85dc70df8b57947bba7ff27e4
jayc545 <jayc545htx@gmail.com>
2022-05-12 10:10:11 +0200

    Last Change

---
+++ Zombie Game/Assets/Scripts/README.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    [TextArea]
+    [Tooltip("Doesn't do anything. Just comments shown in inspector")]
+    public string BareTrykPÂPlay = "This component shouldn't be removed, it does important stuff.";
+}
